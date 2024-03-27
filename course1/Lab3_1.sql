e47ff8ec812ecad5d5d4df67989a6d9701b43657
Nguyễn Đình An <ngdinhan210@gmail.com>
2024-03-27 07:23:22 +0700

    add Lab3_1.sql

---
+++ course1/Lab3_1.sql
@@ -0,0 +1,18 @@
+use cat_app;
+
+create table people(
+	first_name varchar(20),
+    last_name varchar(20),
+    age int
+);
+
+
+insert into people values('Tina', 'Belcher', 13),
+('Bob', 'Belcher', 42),
+('Linda', 'Belcher', 45),
+('Phillip', 'Frond', 38),
+('Calvin', 'Fischoeder', 70);
+
+select * from people;
+drop table people;
+show tables;
